4b3bf3815f7b78baa4c7a33828571fe5b5a01b0f
tekoteko <RamaPapa29@yandex.ru>
2019-10-05 21:48:44 +0300

    Add project files.

---
+++ spa/src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../styles/index.scss';
+
+class App extends React.PureComponent {
+	render() {
+		return (
+			<div>
+				<h1>FodBoy!</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
